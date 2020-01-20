c6f634f81634e5aacb244dd96afa6e096b63cca8
killStrikeFF <43044956+killStrikeFF@users.noreply.github.com>
2020-01-20 22:06:12 +0300

    End of project.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var overlay = document.getElementById('overlay-video');
+var vid = document.getElementById('video');
+
+if(overlay.addEventListener){
+		overlay.addEventListener("click", play, false);
+	}else if(overlay.attachEvent){
+        overlay.attachEvent("onclick", play);
+	}
+
+function play() { 
+    if (vid.paused){
+        vid.play(); 
+        overlay.className = "o";
+    }else {
+        vid.pause(); 
+        overlay.className = "";
+    }
+} 
+
+console.log('Im here');
