758bbdd9fec73f4e8bae02f3218a5a5691ec1929
killStrikeFF <43044956+killStrikeFF@users.noreply.github.com>
2020-03-23 18:09:49 +0300

    Add navigation on site

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 var overlay = document.getElementById('overlay-video');
 var vid = document.getElementById('video');
 
-if(overlay.addEventListener){
-		overlay.addEventListener("click", play, false);
-	}else if(overlay.attachEvent){
-        overlay.attachEvent("onclick", play);
-	}
+if (overlay.addEventListener) {
+    overlay.addEventListener("click", play, false);
+} else if (overlay.attachEvent) {
+    overlay.attachEvent("onclick", play);
+}
 
-function play() { 
-    if (vid.paused){
-        vid.play(); 
+function play() {
+    if (vid.paused) {
+        vid.play();
         overlay.className = "o";
-    }else {
-        vid.pause(); 
+    } else {
+        vid.pause();
         overlay.className = "";
     }
-} 
+}
 
-console.log('Im here');
+$(document).ready(function () {
+
+    $('a[href^="#"]').bind('click.smoothscroll', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+});
